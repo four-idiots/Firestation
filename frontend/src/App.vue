d2222573357bad4a315bfd72e8c34bb653c41d48
E0ho <bbchoi6@hufs.ac.kr>
2022-09-16 10:24:58 +0900

    click event 처리

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,43 +3,39 @@
     <header>화재 감지 서비스</header>
     <nav>
       <router-link
-        @click="change()"
         type="button"
         to="/user/register"
         class="menu-bar"
         id="fire-car"
         >User Register
-        <div><router-view name="view1"/></div>
+        <div class="b"><router-view name="view1" />
+
+        </div>
       </router-link>
+
       <router-link
-        @click="change()"
         type="button"
         to="/admin/register"
         class="menu-bar"
         id="fire-fighter"
         >Admin Register
-        <div><router-view name="view2"/></div>
-        </router-link>
+        <div class="b"><router-view name="view2"/></div>
+      </router-link>
+
       <router-link
-        @click="change()"
         type="button"
         to="/admin/login"
         class="menu-bar"
         id="fire-station"
         >Admin Login
-        <div><router-view name="view3"/></div>
-        </router-link>
+        <router-view name="view3" class="b" />
+      </router-link>
+
     </nav>
   </div>
 </template>
-
 <script>
 export default {
-  method: {
-    change() {
-      const ch = document.getElementsByClassName('menu-bar')
-      console.log(ch)
-    }
-  }
+
 }
 </script>
