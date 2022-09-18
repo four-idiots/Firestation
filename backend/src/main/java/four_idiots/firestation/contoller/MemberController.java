153c99f9e455c11fb8c171c722207ae64827a281
hoyoung2 <pon07060@gmail.com>
2022-09-18 19:49:04 +0900

    thymeleaf 다중경로 설정 추가, vue.js 빌드후 연동 성공

--- backend/src/main/java/four_idiots/firestation/contoller/MemberController.java
+++ backend/src/main/java/four_idiots/firestation/contoller/MemberController.java
@@ -2,41 +2,50 @@ package four_idiots.firestation.contoller;
 
 import four_idiots.firestation.config.auth.MemberDetail;
 import four_idiots.firestation.sevice.FirestationService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpSession;
 
+@Slf4j
 @Controller
 public class MemberController {
+
     @Autowired
     private FirestationService firestationService;
 
     @GetMapping({"", "/"})
     public String index () {
-        return "index.html";
+        return "index";
     }
 
-    @GetMapping("/user/register")
-    public String userRegister() {
-        return "/static/index.html";
-    }
+//    @GetMapping("/user/register")
+//    public String userRegister() {
+//        return "admin.html";
+//    }
 
-    @GetMapping("/admin/register")
-    public String adminRegister() {
-        return "admin.html";
-    }
+//    @GetMapping("/admin/register")
+//    public String adminRegister() {
+//        return "admin.html";
+//    }
 
-    @GetMapping("/auth/loginForm")
-    public String login() {
-        return "loginForm";
-    }
+//    @GetMapping("/auth/loginForm")
+//    public String login() {
+//        return "index";
+//    }
 
-    @GetMapping("/admin/memberList")
-    public String getList(Model model) {
+    @GetMapping("/firestation/memberList")
+    public String getList(Model model, HttpSession session) {
+
         model.addAttribute("memberList", firestationService.userList());
-        return "memberList";
+        System.out.println(session.getId());
+        log.info(session.getId());
+
+        return "memberList.html";
+
     }
 }
